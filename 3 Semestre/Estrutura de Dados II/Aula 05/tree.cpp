ee70f8aed49bbcfdfcfe0dda432572eac1d1182c
breno-olliari <breno.olliari@gmail.com>
2026-03-25 20:57:02 -0300

    Estuto de arvores cpp

---
+++ 3 Semestre/Estrutura de Dados II/Aula 05/tree.cpp
@@ -0,0 +1,32 @@
+//BIblioteca de arvore
+#include <string>
+#include <algorithm>
+
+namespace tree{
+    //struct de cada nodo da árvore
+    struct node{
+        std::string value; //INFORMAÇÃO
+        node *left;  //FILHO DA ESQUERDA
+        node *right; //FILHO DA DIREITA
+    };
+
+    node *create_node(const std::string &str_value){
+        node *p = new node;
+        p->value = str_value;
+        p->left = nullptr;
+        p->right = nullptr;
+
+        return p;
+    }
+
+    //Função que retornar o tamanho da árvore
+    //root é a raiz
+    int height(node *root){
+        if(root == nullptr){
+            return -1;
+        }
+        return std::max(height(root->left),height(root->right))+1;
+
+    }
+
+}
